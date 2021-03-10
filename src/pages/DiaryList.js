bac06b243efdc948ca36dd0110e9d6a4f4af733e
Olga Kulik <kulik_olla@mail.ru>
2021-03-10 14:22:47 +0300

    Added new logic for DiaryList and Create diary

--- src/pages/DiaryList.js
+++ src/pages/DiaryList.js
@@ -1,6 +1,54 @@
-import React from 'react';
-import '../App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAuth } from '../hooks/useAuth';
+import Card from '../components/Cards';
+import { Grid } from '@material-ui/core';
 
-export default function DiaryList() {
-  return <h1 className="diaries"> DiaryList</h1>;
+export default function MyDiaryList() {
+  const { auth } = useAuth();
+  const allUserRecord = async () => {
+    return await axios.get(
+      'https://limitless-savannah-84914.herokuapp.com/api/blog',
+      {
+        headers: {
+          Authorization: 'bearer' + auth.access_token,
+        },
+      }
+    );
+  };
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function req() {
+      try {
+        const response = await allUserRecord();
+        setData(Object.values(response.data));
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    req();
+  }, []);
+
+  return (
+    <div style={{ padding: 12 }}>
+      <Grid container spacing={3}>
+        {data
+          ? data.map((item) => {
+              return (
+                <Grid item xs={6} sm={3} key={item.id}>
+                  <Card
+                    title={item.title}
+                    date={new Date(item.updated_at).toLocaleString('ru')}
+                    description={item.text}
+                    image={''}
+                    avatarUrl="/images/image-8.jpg"
+                  />
+                </Grid>
+              );
+            })
+          : null}
+      </Grid>
+    </div>
+  );
 }
