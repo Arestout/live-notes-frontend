324ddb75f5a7b14641bc10b3e8f9b864a16858f5
shamaev-vlad <vladshamaev@gmail.com>
2021-03-10 18:41:24 +0300

    Добавил адаптива, немного видоизменил карточки

--- src/components/DiariesCardItem.js
+++ src/components/DiariesCardItem.js
@@ -8,18 +8,35 @@ function DiariesCardItem(props) {
   return (
     <>
       <li className="diaries__cards__item">
-        <Link className="cards__item__link" to={props.path}>
-          <figure className="cards__item__pic-wrap">
+        <div className="diaries__item__block">
+          <div className="diaries__header">
+            <div className="header__wrapper">
+              <img
+                className="diaries__item__avatar"
+                alt="Avatar"
+                src={props.avatar}
+              />
+              <div className="diaries__header__block">
+                <div className="diaries__header__heading">
+                  <h5 className="diary__item__title">{props.title}</h5>
+                </div>
+                <div className="pub_date">
+                  <p>{props.date}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="diaries__item__pic-wrap">
             <img
-              className="cards__item__img"
+              className="diaries__item__img"
               alt="User Image"
               src={props.src}
             />
-          </figure>
+          </div>
           <div className="diary__item__info">
-            <h5 className="diary__item__title">{props.title}</h5>
-            <div className="cards__footer">
-              <div>
+            <p className="diary__item__text">{props.text}</p>
+            <div className="diaries__footer">
+              <div className="diaries__icons">
                 <Link
                   className="social_icon_like"
                   to="/"
@@ -37,12 +54,12 @@ function DiariesCardItem(props) {
                   <FontAwesomeIcon icon={faComment} />
                 </Link>
               </div>
-              <div className="pub_date">
-                <p>Дата публикации: {props.date}</p>
-              </div>
+              <Link className="read__more" to="/">
+                Читать далее
+              </Link>
             </div>
           </div>
-        </Link>
+        </div>
       </li>
     </>
   );
--- src/components/DiariesCards.js
+++ src/components/DiariesCards.js
@@ -5,35 +5,62 @@ import DiariesCardItem from './DiariesCardItem';
 function DiariesCards() {
   return (
     <div className="container">
-      <h2 className="diary__header">Дневники</h2>
       <div className="diarycards__container">
         <div className="diarycards__wrapper">
           <ul>
             <DiariesCardItem
               src="images/livenote.jpg"
+              avatar="images/avatar.png"
               title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, adipisci."
-              date="12.02.21"
+              date="12.02.2021"
               path="/services"
+              text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi unde labore non dolorem deserunt. Similique soluta consequatur deleniti voluptate cumque optio voluptatibus dolore, in quisquam voluptatum
+                impedit
+                eveniet ipsa. Quibusdam doloribus nulla laborum provident saepe tenetur ipsum non cum accusamus. Illo sed unde quibusdam ex nihil atque necessitatibus cumque ut eaque explicabo aliquam, est, dicta sequi in quod minima laborum
+                voluptas,
+                voluptatum vel harum eveniet labore obcaecati. Inventore quod fugit sequi suscipit quaerat, aspernatur nemo cum qui facilis. Magnam rerum tempore enim corrupti molestias, totam rem magni molestiae. Eveniet consequuntur dolores
+                quaerat"
             />
             <DiariesCardItem
               src="images/livenote.jpg"
+              avatar="images/avatar.png"
               title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, adipisci."
-              date="12.02.21"
+              date="12.02.2021"
               path="/services"
+              text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi unde labore non dolorem deserunt. Similique soluta consequatur deleniti voluptate cumque optio voluptatibus dolore, in quisquam voluptatum
+                impedit
+                eveniet ipsa. Quibusdam doloribus nulla laborum provident saepe tenetur ipsum non cum accusamus. Illo sed unde quibusdam ex nihil atque necessitatibus cumque ut eaque explicabo aliquam, est, dicta sequi in quod minima laborum
+                voluptas,
+                voluptatum vel harum eveniet labore obcaecati. Inventore quod fugit sequi suscipit quaerat, aspernatur nemo cum qui facilis. Magnam rerum tempore enim corrupti molestias, totam rem magni molestiae. Eveniet consequuntur dolores
+                quaerat"
             />
           </ul>
           <ul>
             <DiariesCardItem
               src="images/livenote.jpg"
+              avatar="images/avatar.png"
               title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, adipisci."
-              date="12.02.21"
+              date="12.02.2021"
               path="/services"
+              text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi unde labore non dolorem deserunt. Similique soluta consequatur deleniti voluptate cumque optio voluptatibus dolore, in quisquam voluptatum
+                impedit
+                eveniet ipsa. Quibusdam doloribus nulla laborum provident saepe tenetur ipsum non cum accusamus. Illo sed unde quibusdam ex nihil atque necessitatibus cumque ut eaque explicabo aliquam, est, dicta sequi in quod minima laborum
+                voluptas,
+                voluptatum vel harum eveniet labore obcaecati. Inventore quod fugit sequi suscipit quaerat, aspernatur nemo cum qui facilis. Magnam rerum tempore enim corrupti molestias, totam rem magni molestiae. Eveniet consequuntur dolores
+                quaerat"
             />
             <DiariesCardItem
               src="images/livenote.jpg"
+              avatar="images/avatar.png"
               title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, adipisci."
-              date="12.02.21"
+              date="12.02.2021"
               path="/services"
+              text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi unde labore non dolorem deserunt. Similique soluta consequatur deleniti voluptate cumque optio voluptatibus dolore, in quisquam voluptatum
+                impedit
+                eveniet ipsa. Quibusdam doloribus nulla laborum provident saepe tenetur ipsum non cum accusamus. Illo sed unde quibusdam ex nihil atque necessitatibus cumque ut eaque explicabo aliquam, est, dicta sequi in quod minima laborum
+                voluptas,
+                voluptatum vel harum eveniet labore obcaecati. Inventore quod fugit sequi suscipit quaerat, aspernatur nemo cum qui facilis. Magnam rerum tempore enim corrupti molestias, totam rem magni molestiae. Eveniet consequuntur dolores
+                quaerat"
             />
           </ul>
         </div>
